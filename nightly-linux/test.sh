8d9bc1c06bd3767b44fae14b9d4951c5f9700af0
Michka Popoff <michkapopoff@gmail.com>
2022-05-16 21:15:45 +0200

    test

--- nightly-linux/test.sh
+++ nightly-linux/test.sh
@@ -5,3 +5,17 @@ brew test hello
 brew uninstall hello
 brew install -s hello
 brew test hello
+
+
+export HOMEBREW_DEVELOPER=1
+brew remove --force $(brew list --formula)
+cd /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core
+git remote add sjackman https://github.com/sjackman/homebrew-core
+git fetch sjackman
+git checkout glibc-2.13
+
+brew install hello
+brew test hello
+brew uninstall hello
+brew install -s hello
+brew test hello
